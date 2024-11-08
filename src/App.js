ed44127e9b194d9fbf0f3f655d7eaa416627ebfd
unknown <Saahil Sood>
2024-11-08 00:05:38 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom'
+import Home from './containers/home';
+import About from './containers/about';
+import Contact from './containers/contact';
+import Portfolio from './containers/portfolio';
+import Resume from './containers/resume';
+import Skills from './containers/skills';
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route index path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/resume" element={<Resume />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
